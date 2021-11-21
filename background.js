cad5cccd088c618ca5242b8a25cb4939d21c1eaf
Nicholas-Lin <nicholaslin10@gmail.com>
2021-11-20 22:02:49 -0500

    Add domain list

--- background.js
+++ background.js
@@ -1,5 +1,24 @@
+// domain_patterns = blocked_domains.map((domain) => {
+//   try{
+//     const pattern = new URLPattern({
+//       hostname: domain,
+//     });
+//     return pattern;
+//   }catch(e){
+//     console.error(e)
+//   }
+// })
+// console.log(domain_patterns)
+
 function logURL(requestDetails) {
-  console.log(requestDetails.initiator, requestDetails.url);
+  // initiator = requestDetails.initiator;
+  // url = requestDetails.url;
+  // domain_patterns.forEach(function(domain){
+  //     if(domain.test(url)){
+  //       console.log("MATCH", url, host)
+  //     }
+  // })
+  console.log(requestDetails);
 }
 
 function cancel(requestDetails) {
@@ -7,4 +26,4 @@ function cancel(requestDetails) {
   return { cancel: true };
 }
 
-chrome.webRequest.onBeforeRequest.addListener(logURL, { urls: ["<all_urls>"] });
+chrome.webRequest.onBeforeRequest.addListener(logURL, { urls: blocked_domains }, ['blocking']);
