abd4b400745435001abdc20bf594add76a1f53a1
etulsian <67609320+etulsian@users.noreply.github.com>
2021-12-01 14:07:19 -0500

    Updated the html and js to show current website

--- popup.js
+++ popup.js
@@ -1,13 +1,12 @@
 window.onload = function () {
   // Getting the current domain
+
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var tab = tabs[0];
     var url = new URL(tab.url)
     var domain = url.hostname
     console.log(domain)
+    document.getElementById("current_website").innerHTML = "Current Website: " + domain;
   });
+  
 }
-
-
-
-
