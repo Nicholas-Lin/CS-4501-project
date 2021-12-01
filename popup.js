ad8ab5f3b684853f54b87e6401ff6ebf38f1e885
Nicholas-Lin <nicholaslin10@gmail.com>
2021-12-01 01:07:50 -0500

    Update comment

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ window.onload = function () {
 		document.getElementById('toggle_extension').value = enabled ? "Disable Extension" : "Enable Extension";
 	}
 
-  // Getting the current URL
+  // Getting the current domain
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var tab = tabs[0];
     var url = new URL(tab.url)
